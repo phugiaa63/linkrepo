33356a85930548e928ac8736d12e2886dc8b69ef
phugia63 <ndrapheal2003@gmail.com>
2025-05-22 19:57:48 +0700

    push thêm file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./src/config/database');
 const redirectRoute = require('./src/routes/redirectRoute');
-
+const landingRoute = require('./src/routes/landingRoute');
 require('dotenv').config(); // Để đọc biến môi trường từ .env
 
 const app = express();
@@ -12,6 +12,7 @@ app.use(cors())
 connectDB();
 
 app.use('/', redirectRoute);
+app.use('/landing', landingRoute);
 
 app.listen(PORT, () => {
   console.log(`Server chạy tại http://localhost:${PORT}`);
