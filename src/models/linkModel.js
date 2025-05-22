ab2b836b733431e2e929f8fcc0de261226d972c3
phugia63 <ndrapheal2003@gmail.com>
2025-05-22 20:05:45 +0700

    them route

--- src/models/linkModel.js
+++ src/models/linkModel.js
@@ -1,7 +1,7 @@
 // Dữ liệu link bọc tạm thời (thay thế bằng DB sau này)
 const linkMap = {
   'abc123': 'https://kisxgs.md1jik01.com/home/?inviteCode=1837247#/',
-  'xyz456': 'https://kisxgs.md1jik01.com/home/?inviteCode=1837247#/'
+  'xyz456': 'https://playy-hjt.club/'
 };
 
 function getOriginalUrl(code) {
