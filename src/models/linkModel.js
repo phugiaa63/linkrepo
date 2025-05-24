677f08d9a0b3e780690c610aac1acc02864a4e6b
phugia63 <ndrapheal2003@gmail.com>
2025-05-24 12:50:11 +0700

    thay doi link

--- src/models/linkModel.js
+++ src/models/linkModel.js
@@ -1,7 +1,7 @@
 const linkMap = {
   'abc123': {
-    mobile: 'https://www.webcake.me/demo/v4/8a75a949-59a1-4a4e-91d7-4290216fa88f?locale=vi',
-    desktop: 'https://www.webcake.me/demo/v4/236ed95d-e6bd-43af-a7c4-f88131bd7c0c?locale=vi'
+    mobile: 'https://www.webcake.me/demo/v4/8a75a949-59a1-4a4e-91d7-4290216fa88f',
+    desktop: 'https://www.webcake.me/demo/v4/236ed95d-e6bd-43af-a7c4-f88131bd7c0c'
   }
 };
 
